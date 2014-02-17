e51d9e4e4b46d3dd69a71ac67c69f30935041324
zsky <kabsky9@gmail.com>
2014-02-17 20:26:44 +0800

    connect

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ SECRET_KEY = 'you_will_never_guess'
 
 # 这是我的本地数据库配置
 #SQLALCHEMY_DATABASE_URI = 'mysql://root:hs1019@localhost/yearbook' 
-SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
+SQLALCHEMY_DATABASE_URI = os.environ['HEROKU_POSTGRESQL_BRONZE_URL']
 
 # upload files setting
 UPLOAD_FOLDER = os.path.join(os.getcwd(), 'app/static/upload')
